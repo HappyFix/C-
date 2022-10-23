ad7c9ca21517d1edb48fb218f58ecbd78ca6d6f5
HappyFix <gitinishn@gmail.com>
2022-10-23 19:20:39 +0300

    add hw2

---
+++ hw2/task10/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+
+int myNumber = MyInt("Введите трехзначное число: ");
+int sizeNumber = myNumber.ToString().Length;
+
+if (sizeNumber < 3 || sizeNumber > 3)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+    Console.WriteLine(Target(myNumber));
+}
+
+int MyInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Target(int a)
+{
+    int result = ((a / 10) % 10);
+    return result;
+}
---
+++ hw2/task13/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int myNumber = Convert.ToInt32(Console.ReadLine());
+string target = Math.Abs(myNumber).ToString();
+
+Console.WriteLine(target.Length > 2 ? target[2] : "Третьей цифры нет");
