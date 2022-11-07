c688c486c5528f58474083fc5d0f1770d5e872b7
HappyFix <gitinishn@gmail.com>
2022-11-08 00:19:33 +0300

    add hw4

---
+++ hw4/task27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int target = Convert.ToInt32(Console.ReadLine());
+int length = target < 0 ? target.ToString().Length - 1 : target.ToString().Length;
+
+void SumNumbers(int n, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine(sum);
+}
+
+SumNumbers(target, length);
